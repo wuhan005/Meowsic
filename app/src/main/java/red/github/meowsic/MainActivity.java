65e64de3276be45a7e9f4dd2280af96b928b8677
John Wu <524306184@qq.com>
2019-06-04 18:33:11 +0800

    顶部菜单

--- app/src/main/java/red/github/meowsic/MainActivity.java
+++ app/src/main/java/red/github/meowsic/MainActivity.java
@@ -2,6 +2,7 @@ package red.github.meowsic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // 顶部右侧菜单
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.header, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
