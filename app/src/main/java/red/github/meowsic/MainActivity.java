a76ed350568e45593b0e7ddd709c95d5407b17d9
John Wu <524306184@qq.com>
2019-06-05 19:52:51 +0800

    搜索页面

--- app/src/main/java/red/github/meowsic/MainActivity.java
+++ app/src/main/java/red/github/meowsic/MainActivity.java
@@ -1,8 +1,10 @@
 package red.github.meowsic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.header, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    // 选择菜单选项
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            // 搜索按钮
+            case R.id.search:
+                Intent searchIntent = new Intent();
+                searchIntent.setClass(this, SearchActivity.class);
+                startActivity(searchIntent);
+                break;
+            // 关于按钮
+            case R.id.about:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
