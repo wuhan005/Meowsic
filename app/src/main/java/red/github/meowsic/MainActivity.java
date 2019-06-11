5c86bd9352bb4a4e4ca16cd0894858a385128619
John Wu <524306184@qq.com>
2019-06-11 15:50:14 +0800

    播放历史

--- app/src/main/java/red/github/meowsic/MainActivity.java
+++ app/src/main/java/red/github/meowsic/MainActivity.java
@@ -5,15 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private JSONArray historyList;
+    private ListView musicList;
+    private List<MusicItem> musicItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.musicList = findViewById(R.id.musicList);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         this.getList();
     }
 
@@ -42,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getList(){
+        this.musicItem = new ArrayList<>();
         String fileName = "play_history.txt";
 
         try {
@@ -50,13 +69,68 @@ public class MainActivity extends AppCompatActivity {
             int length = fis.available();       // Get the length of the file.
             byte[] buffer = new byte[length];
             fis.read(buffer);
-            System.out.println("Text:::");
             String content = new String(buffer, "UTF-8");
-            System.out.println(content);
+
+            historyList = new JSONArray(content);
+
+            this.setList();
 
         }catch (Exception e){
-            System.out.println("Out put:");
             System.out.println(e);
         }
+
+    }
+
+    private void setList(){
+        try{
+            System.out.println(this.historyList);
+            for(int i = 0; i < this.historyList.length(); i++){
+                JSONObject item = historyList.getJSONObject(this.historyList.length() - 1 - i);
+                musicItem.add(new MusicItem(
+                        item.getString("musicName"),
+                        item.getString("artistName"),
+                        item.getString("albumName"),
+                        item.getString("albumPicture"),
+                        item.getString("mid")
+                ));
+            }
+
+            MusicAdapter adapter = new MusicAdapter(this, R.layout.music_item, musicItem);
+            this.musicList.setAdapter(adapter);
+
+        }catch (Exception e){
+            System.out.println("Error!!!");
+        }
+
+        musicList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Redirect to the music player page.
+                Intent playerIntent = new Intent();
+                playerIntent.setClass(MainActivity.this, MusicPlayer.class);
+
+                playerIntent.putExtra("mid", musicItem.get((int) id).getMid());      // Send the music mid.
+                playerIntent.putExtra("musicName", musicItem.get((int) id).getMusicName());
+                playerIntent.putExtra("albumName", musicItem.get((int) id).getAlbumName());
+                playerIntent.putExtra("artistName", musicItem.get((int) id).getMusicArtist());
+                playerIntent.putExtra("albumPicture", musicItem.get((int) id).getAlbumPicture());
+
+                // Save history
+
+                JSONObject historyObject = new JSONObject();
+                try {
+                    historyObject.put("mid", musicItem.get((int) id).getMid());
+                    historyObject.put("musicName", musicItem.get((int) id).getMusicName());
+                    historyObject.put("albumName", musicItem.get((int) id).getAlbumName());
+                    historyObject.put("artistName", musicItem.get((int) id).getMusicArtist());
+                    historyObject.put("albumPicture", musicItem.get((int) id).getAlbumPicture());
+                }catch (Exception e){
+                    System.out.println(e);
+                }
+
+                startActivity(playerIntent);
+            }
+        });
+
     }
 }
